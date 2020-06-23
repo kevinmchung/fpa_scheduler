218434092835a8b9baef9147622195851b57e353
kevinchung888 <kevinchung888@yahoo.com>
2020-06-23 15:03:32 -0400

    initial commit

---
+++ source/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\David\Google Drive\projects\scheduler\source\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

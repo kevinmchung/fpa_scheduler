246ff9c84cc37ef2f6e8d6bc57139d022a754166
MarketMonkey <heydavidly@gmail.com>
2020-06-25 10:53:57 -0400

    added Name model to Scheduler

--- source/scheduler/models.py
+++ source/scheduler/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Name(models.Model):
+    name_first = models.CharField(max_length=50)
+    name_last = models.CharField(max_length=50)

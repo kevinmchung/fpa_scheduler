f372704594b2129b04ffc35f06a17381f3f300bf
MarketMonkey <heydavidly@gmail.com>
2020-06-25 10:21:35 -0400

    Template Fixes
    Fixed templates structure

--- source/mysite/settings.py
+++ source/mysite/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'scheduler.apps.SchedulerConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',

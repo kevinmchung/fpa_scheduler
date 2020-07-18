2a1cb7a6747684b1105f9dddecd7a36314e17872
MarketMonkey <heydavidly@gmail.com>
2020-07-18 14:02:44 -0400

    added names list to admin

--- source/scheduler/models.py
+++ source/scheduler/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Name(models.Model):
     name_first = models.CharField(max_length=50)
     name_last = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} {}".format(self.name_first, self.name_last)
--- source/scheduler/views.py
+++ source/scheduler/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
+from .models import Name
+
 
 class IndexView(generic.ListView):
     template_name = 'scheduler/index.html'
@@ -12,8 +14,12 @@ class IndexView(generic.ListView):
         return 0
 
 
+
 class ListView(generic.ListView):
     template_name = 'scheduler/list.html'
+    context_object_name = 'names_list'
 
     def get_queryset(self):
-        return 0
+        return Name.objects.order_by('name_last')
+
+

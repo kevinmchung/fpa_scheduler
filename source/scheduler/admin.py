b1cf70b9c6b1de69021eed5a5e60845f4f1dc581
kevinchung888 <kevinchung888@yahoo.com>
2020-07-19 17:55:45 -0400

    added provider models

--- source/scheduler/admin.py
+++ source/scheduler/admin.py
@@ -1,3 +1,8 @@
 # Register your models here.
 
 from django.contrib import admin
+
+from .models import Provider, Office
+
+admin.site.register(Provider)
+admin.site.register(Office)
--- source/scheduler/models.py
+++ source/scheduler/models.py
@@ -14,3 +14,25 @@ class Name(models.Model):
         return "{} {}".format(self.name_first, self.name_last)
 
 '''
+
+class Provider(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	abbrev = models.CharField(max_length=3)
+
+	num_work_days = models.TextField()
+	vacation_days = models.TextField()
+	office_ranges = models.TextField()
+
+	def __str__(self):
+		return "{}, {} ({})".format(self.last_name, self.first_name, self.abbrev)
+
+class Office(models.Model):
+	name = models.CharField(max_length=50)
+	abbrev = models.CharField(max_length=10)
+
+	provider_range = models.TextField()
+	weekend = models.BooleanField()
+
+	def __str__(self):
+		return "{} ({})".format(self.name, self.abbrev)

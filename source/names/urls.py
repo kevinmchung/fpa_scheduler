970ad88ba8c3fd3ba5073fd23b48ac1a01851b8b
MarketMonkey <heydavidly@gmail.com>
2020-07-20 14:33:35 -0400

    generic views

--- source/names/urls.py
+++ source/names/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = 'names'
 
 urlpatterns = [
-    #path('', views.IndexView.as_view(), name='index'),
-    path('', views.name_add, name='add'),
 
+    path('', views.IndexView.as_view(), name='name-index'),
+    path('<int:pk>', views.DetailView.as_view(), name='name-detail'),
+    path('<int:pk>/update', views.NameUpdate.as_view(), name='name-update'),
 
-    path('<int:pk>/update', views.NameUpdate.as_view(), name='update'),
-    #path('<int:pk>/edit', views.EditView.as_view(), name='edit'),
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('', views.name_add, name='add'),
+    # path('<int:pk>/edit', views.EditView.as_view(), name='edit'),
 
 ]
--- source/names/views.py
+++ source/names/views.py
@@ -6,14 +6,51 @@ from .forms import NameForm
 from django.views.generic.edit import UpdateView
 
 
-'''
+
 class IndexView(generic.ListView):
     template_name = 'names/index.html'
     context_object_name = 'names_list'
 
-    def get_queryset(self):
-        return Name.objects.order_by('name_last')
+    form_class = NameForm
+
+    def get(self, request):
+        #form_class = NameForm
+        return render(request, 'names/index.html', {'form': self.form_class, 'names_list': Name.objects.order_by('name_last')})
+
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
+        return render(request, self.template_name, {'form': form})
+
+
+
+class DetailView(generic.DetailView):
+    model = Name
+    template_name = 'names/detail.html'
+
+
+class NameUpdate(UpdateView):
+    model = Name
+    fields = ['name_first', 'name_last']
+    template_name = template_name = 'names/detail.html'
+
+
 '''
+
+    def get(self, request):
+        template_name = 'polls/detail.html'
+
+'''
+
+'''
+class ResultsView(generic.DetailView):
+    model = Names
+    template_name = 'names/results.html'
+
+
 
 def name_add(request):
 
@@ -34,27 +71,24 @@ def name_add(request):
     return render(request, 'names/index.html', {'form': form, 'names_list': Name.objects.order_by('name_last')})
 
 '''
+'''
 class EditView(generic.ListView):
     template_name = 'names/edit.html'
     context_object_name = 'names_list'
 
     def get_queryset(self):
         return Name.objects.order_by('name_last')
-'''
-'''
+
 class EditView(generic.DetailView):
     model = Name
     template_name = 'names/edit.html'
+
 '''
 
 
 
 
 
-class NameUpdate(UpdateView):
-    model = Name
-    fields = ['name_first', 'name_last']
-    template_name_suffix = '_update'
 
 
 
